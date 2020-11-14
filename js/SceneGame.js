6a93d1845c223b9e538976ab3a81f90fe1d3d7d5
L. K <LK00100100@gmail.com>
2020-11-14 02:23:34 -0500

    winner check works

--- js/SceneGame.js
+++ js/SceneGame.js
@@ -173,6 +173,11 @@ class SceneGame extends Phaser.Scene {
             scene.ghostYellowSprites[col].visible = false;
     }
 
+    /**
+     * attempts to place a piece down in the col (for current player) and draws it.
+     * On an invalid, nothing happens.
+     * @param {Number} col the target column
+     */
     attemptMoveAndDraw(col) {
         let rowPlaced = this.placePiece(col, this.currentPlayer);
 
@@ -181,6 +186,12 @@ class SceneGame extends Phaser.Scene {
 
         this.animatePlacePiece(rowPlaced, col, this.currentPlayer);
 
+        let isWinner = this.checkVictoryQuick(rowPlaced, col);
+
+        if (isWinner)
+            console.log(isWinner);
+
+        //END OF TURN
         //change player and change ghost
         if (this.currentPlayer == 1) {
             this.currentPlayer = 2;
@@ -268,8 +279,127 @@ class SceneGame extends Phaser.Scene {
         this.pieceSprites.push(newPiece);
     }
 
-    checkVictory() {
+    /**
+     * Quickly check if the newly placed piece created a victory.
+     * @param {Number} row the newly placed piece
+     * @param {Number} col the newly placed piece
+     */
+    checkVictoryQuick(row, col) {
+        let player = this.board[row][col];
 
+        if (this.checkHorizontalQuick(row, col - 3, player)) //to left
+            return true;
 
+        if (this.checkHorizontalQuick(row, col, player)) //to right
+            return true;
+
+        if (this.checkVerticalQuick(row - 3, col, player)) //to up
+            return true;
+
+        if (this.checkVerticalQuick(row, col, player)) //to down
+            return true;
+
+        if (this.checkDiagonalSEQuick(row, col, player))
+            return true;
+
+        if (this.checkDiagonalNEQuick(row, col, player))
+            return true;
+
+        if (this.checkDiagonalSEQuick(row - 3, col - 3, player))
+            return true;
+
+        if (this.checkDiagonalNEQuick(row + 3, col - 3, player))
+            return true;
+    }
+
+    /**
+     * Checks four horizontal from left to right
+     * @param {Number} row 
+     * @param {Number} col 
+     * @param {Number} player 
+     */
+    checkHorizontalQuick(row, col, player) {
+        for (let i = 0; i < 4; i++) {
+            if (this.isOutOfBounds(row + i, col))
+                return false;
+
+            if (this.board[row + i][col] != player)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks four vertical from top to bottom
+     * @param {Number} row 
+     * @param {Number} col 
+     * @param {Number} player 
+     * @returns {Boolean} 
+     */
+    checkVerticalQuick(row, col, player) {
+        for (let i = 0; i < 4; i++) {
+            if (this.isOutOfBounds(row, col + i))
+                return false;
+
+            if (this.board[row][col + i] != player)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+    * Checks four diagonal towards south-east
+    * @param {Number} row 
+    * @param {Number} col 
+    * @param {Number} player 
+    * @returns {Boolean} 
+    */
+    checkDiagonalSEQuick(row, col, player) {
+        for (let i = 0; i < 4; i++) {
+            if (this.isOutOfBounds(row + i, col + i))
+                return false;
+
+            if (this.board[row + i][col + i] != player)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks four diagonal towards north-east
+     * @param {Number} row 
+     * @param {Number} col 
+     * @param {Number} player 
+     * @returns {Boolean} 
+     */
+    checkDiagonalNEQuick(row, col, player) {
+        for (let i = 0; i < 4; i++) {
+            if (this.isOutOfBounds(row - i, col + i))
+                return false;
+
+            if (this.board[row - i][col + i] != player)
+                return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 
+     * @param {Number} row 
+     * @param {Number} col 
+     * @returns {boolean} true if out of bounds.
+     */
+    isOutOfBounds(row, col) {
+        if (row < 0 || row >= this.board.length)
+            return true;
+
+        if (col < 0 || col >= this.board[0].length)
+            return true;
+
+        return false;
     }
 }
